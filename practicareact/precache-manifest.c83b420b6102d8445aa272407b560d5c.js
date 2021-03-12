a77c4e45558f65ad40a834ec2652dbd79670434a
Edgar24k <edgarlozoya24k@gmail.com>
2021-03-12 13:23:16 -0600

    commit

---
+++ practicareact/precache-manifest.c83b420b6102d8445aa272407b560d5c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657a450c7f1a93295a73ac93ca4d3dda",
+    "url": "/practicareact/index.html"
+  },
+  {
+    "revision": "dd2eef1dab8a1fc41b93",
+    "url": "/practicareact/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "8a98c773f080c055437e",
+    "url": "/practicareact/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dd2eef1dab8a1fc41b93",
+    "url": "/practicareact/static/js/2.493321b0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/practicareact/static/js/2.493321b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a98c773f080c055437e",
+    "url": "/practicareact/static/js/main.cfaddfb6.chunk.js"
+  },
+  {
+    "revision": "f335c93278f5f8f2528b",
+    "url": "/practicareact/static/js/runtime-main.147f70eb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/practicareact/static/media/logo.5d5d9eef.svg"
+  }
+]);
